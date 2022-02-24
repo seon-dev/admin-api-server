4eeaee81f68bc023eeb2aba6226c984ba4d14dd3
seoeun1221 <ipe1221@plavcorp.com>
2022-02-24 12:57:37 +0900

    fix: getAllAssetPrototype 에러 수정중

---
+++ domain/src/main/java/server/admin/model/asset/dto/response/AssetLineResponse.java
@@ -0,0 +1,2 @@
+package server.admin.model.asset.dto.response;public class AssetLineResponse {
+}
---
+++ domain/src/main/java/server/admin/model/asset/repository/AssetBrandCategoryRepositoryImpl.java
@@ -0,0 +1,2 @@
+package server.admin.model.asset.repository;public class AssetBrandCategoryRepositoryImpl {
+}
