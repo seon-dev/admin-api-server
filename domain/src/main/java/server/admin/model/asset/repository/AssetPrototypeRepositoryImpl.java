3d6fe62cc15be4f67ebcdb98fb2f3a13aa6e2e36
seoeun1221 <ipe1221@plavcorp.com>
2022-02-26 14:20:32 +0900

    fix: getAllAssetPrototype 조회에러 수정 완료

--- domain/src/main/java/server/admin/model/asset/repository/AssetPrototypeRepositoryImpl.java
+++ domain/src/main/java/server/admin/model/asset/repository/AssetPrototypeRepositoryImpl.java
@@ -16,6 +16,7 @@ import server.admin.model.common.QueryDslSupport;
 
 import javax.persistence.EntityManager;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -45,16 +46,7 @@ public class AssetPrototypeRepositoryImpl
 
     @Override
     public Page<AssetPrototypeResponse> getAllAssetPrototype(Pageable pageable, Boolean isEnabled){
-        JPAQuery<AssetPrototype> query = queryFactory.selectFrom(assetPrototype)
-                .where(
-                        checkEnabled(isEnabled)
-                )
-                .leftJoin(assetPrototype.brand, QBrand.brand)
-                .leftJoin(assetPrototype.line, QAssetLine.assetLine)
-                .leftJoin(assetPrototype.brandCategory, QAssetBrandCategory.assetBrandCategory)
-                .leftJoin(assetPrototype.season, QAssetSeason.assetSeason);
-
-        List<AssetPrototype> fetch = queryFactory.selectFrom(assetPrototype)
+        List<AssetPrototype> assetPrototypeList = queryFactory.selectFrom(assetPrototype)
                 .where(
                         checkEnabled(isEnabled)
                 )
@@ -66,8 +58,6 @@ public class AssetPrototypeRepositoryImpl
                 .leftJoin(assetPrototype.season, QAssetSeason.assetSeason)
                 .fetch();
 
-        System.out.println(fetch.size());
-
         Long count = queryFactory.select(assetPrototype.count())
                 .from(assetPrototype)
                 .where(
@@ -76,33 +66,37 @@ public class AssetPrototypeRepositoryImpl
                 )
                 .fetchOne();
 
+        List<AssetPrototypeResponse> assetPrototypeResponseList = new ArrayList<>();
+        assetPrototypeList.forEach(assetPrototype -> {
+            assetPrototypeResponseList.add(AssetPrototypeResponse.toResponse(assetPrototype));
+        });
 
 //       Objects.requireNonNull(getQuerydsl()).applyPagination(pageable, query)
 
-        List<AssetPrototypeResponse> assetPrototypeResponseList = query
-                .select(Projections.constructor(AssetPrototypeResponse.class,
-                        assetPrototype.id,
-                        projectBrandResponse(),
-//                        null,
-                        projectAssetLine(),
-                        projectAssetSeason(),
-                        projectBrandCategory(),
-                        assetPrototype.name,
-                        assetPrototype.decorator,
-                        assetPrototype.code,
-                        assetPrototype.description,
-                        assetPrototype.releasePrice,
-                        assetPrototype.additional,
-                        assetPrototype.trendy,
-                        assetPrototype.likes,
-                        assetPrototype.keywords,
-                        assetPrototype.resourceFront,
-                        assetPrototype.resourceAdditional,
-                        assetPrototype.resourceRear,
-                        assetPrototype.resourceSide,
-                        assetPrototype.isEnabled
-                ))
-                .fetch();
+//        List<AssetPrototypeResponse> assetPrototypeResponseList = query
+//                .select(Projections.constructor(AssetPrototypeResponse.class,
+//                        assetPrototype.id,
+//                        projectBrandResponse(),
+////                        null,
+//                        projectAssetLine(),
+//                        projectAssetSeason(),
+//                        projectBrandCategory(),
+//                        assetPrototype.name,
+//                        assetPrototype.decorator,
+//                        assetPrototype.code,
+//                        assetPrototype.description,
+//                        assetPrototype.releasePrice,
+//                        assetPrototype.additional,
+//                        assetPrototype.trendy,
+//                        assetPrototype.likes,
+//                        assetPrototype.keywords,
+//                        assetPrototype.resourceFront,
+//                        assetPrototype.resourceAdditional,
+//                        assetPrototype.resourceRear,
+//                        assetPrototype.resourceSide,
+//                        assetPrototype.isEnabled
+//                ))
+//                .fetch();
 
 //        List<AssetPrototypeResponse> assetPrototypeResponseList = query.select(new QAssetPrototypeResponse(
 //                assetPrototype.id,
@@ -126,14 +120,13 @@ public class AssetPrototypeRepositoryImpl
 //                assetPrototype.isEnabled
 //        )).fetch();
 
-        System.out.println(assetPrototypeResponseList.size());
-//        return assetPrototypeResponseList;
 
         return new PageImpl<>(assetPrototypeResponseList, pageable, count);
     }
 
 //    private Field getFieldValue(String fieldName, String search) throws NoSuchFieldException {
-//        // 1. field value
+//        <reflection 로직 순서>
+//          1. field value
 //        // 2. field value에 해당하는 매서드 => is
 //        // 3. is.call(search)
 //        return user.getClass().getDeclaredField(fieldName);
@@ -142,57 +135,59 @@ public class AssetPrototypeRepositoryImpl
     private BooleanExpression checkEnabled(Boolean isEnabled){
         return isEnabled != null ? assetPrototype.isEnabled.eq(isEnabled) : null;
     }
+//
+//    private ConstructorExpression<BrandResponse.Minified> projectBrandResponse(){
+//        ConstructorExpression<BrandResponse.Minified> constructor = Projections.constructor(BrandResponse.Minified.class,
+//                assetPrototype.brand.id,
+//                assetPrototype.brand.name,
+//                assetPrototype.brand.originalName,
+//                assetPrototype.brand.resource
+//        );
+//        System.out.println("brand "+ constructor);
+//        return constructor;
+//    }
+//
+//    private ConstructorExpression<AssetLineResponse.Minified> projectAssetLine(){
+//        ConstructorExpression<AssetLineResponse.Minified> constructor = Projections.constructor(AssetLineResponse.Minified.class,
+//                assetPrototype.line.id,
+//                assetPrototype.brand.id,
+//                assetPrototype.brandCategory.id,
+//                assetPrototype.line.name,
+//                assetPrototype.line.resource,
+//                assetPrototype.line.isEnabled
+//        );
+//        System.out.println("assetLine "+ constructor);
+//        return constructor;
+//    }
+//
+//    private ConstructorExpression<AssetSeasonResponse> projectAssetSeason(){
+//        ConstructorExpression<AssetSeasonResponse> constructor = Projections.constructor(AssetSeasonResponse.class,
+//                assetPrototype.season.id,
+//                assetPrototype.season.name,
+//                assetPrototype.season.resource,
+//                assetPrototype.season.isEnabled
+//        );
+//        System.out.println("assetSeason "+constructor);
+//        return constructor;
+//    }
+//
+//    private ConstructorExpression<AssetBrandCategoryResponse.Minified> projectBrandCategory(){
+//        ConstructorExpression<AssetBrandCategoryResponse.Minified> constructor = Projections.constructor(AssetBrandCategoryResponse.Minified.class,
+//                assetPrototype.brandCategory.id,
+//                assetPrototype.brandCategory.brand.id,
+//                assetPrototype.brandCategory.category.id,
+//                assetPrototype.brandCategory.category.name,
+//                assetPrototype.brandCategory.isEnabled
+//        );
+//        System.out.println("brandCategory "+constructor);
+//        return constructor;
+//    }
 
-    private ConstructorExpression<BrandResponse.Minified> projectBrandResponse(){
-        ConstructorExpression<BrandResponse.Minified> constructor = Projections.constructor(BrandResponse.Minified.class,
-                assetPrototype.brand.id,
-                assetPrototype.brand.name,
-                assetPrototype.brand.originalName,
-                assetPrototype.brand.resource
-        );
-        System.out.println("brand "+ constructor);
-        return constructor;
-    }
 
-    private ConstructorExpression<AssetLineResponse.Minified> projectAssetLine(){
-        ConstructorExpression<AssetLineResponse.Minified> constructor = Projections.constructor(AssetLineResponse.Minified.class,
-                assetPrototype.line.id,
-                assetPrototype.brand.id,
-                assetPrototype.brandCategory.id,
-                assetPrototype.line.name,
-                assetPrototype.line.resource,
-                assetPrototype.line.isEnabled
-        );
-        System.out.println("assetLine "+ constructor);
-        return constructor;
-    }
-
-    private ConstructorExpression<AssetSeasonResponse> projectAssetSeason(){
-        ConstructorExpression<AssetSeasonResponse> constructor = Projections.constructor(AssetSeasonResponse.class,
-                assetPrototype.season.id,
-                assetPrototype.season.name,
-                assetPrototype.season.resource,
-                assetPrototype.season.isEnabled
-        );
-        System.out.println("assetSeason "+constructor);
-        return constructor;
-    }
-
-    private ConstructorExpression<AssetBrandCategoryResponse.Minified> projectBrandCategory(){
-        ConstructorExpression<AssetBrandCategoryResponse.Minified> constructor = Projections.constructor(AssetBrandCategoryResponse.Minified.class,
-                assetPrototype.brandCategory.id,
-                assetPrototype.brandCategory.brand.id,
-                assetPrototype.brandCategory.category.id,
-                assetPrototype.brandCategory.category.name,
-                assetPrototype.brandCategory.isEnabled
-        );
-        System.out.println("brandCategory "+constructor);
-        return constructor;
-    }
-
+    //정렬조건을 동적으로 가능하게 하기
 //        private OrderSpecifier<Long> OrderByAsc(String orderBy){
 
-        //정렬조건을 동적으로 가능하게 하기
+
 //        if(Objects.equals(orderBy, "id")) return userAssetApplication.id.asc();
 //        if(Objects.equals(orderBy, "asset")) return userAssetApplication.asset.id.asc();
 //        ...
