08fa8c2a4c5c38541f2ecacb86706f4caba341f1
seoeun1221 <ipe1221@plavcorp.com>
2022-03-29 14:37:08 +0900

    feat: report-update api 추가

--- domain/src/main/java/server/admin/model/report/dto/request/AppReportUpdateRequest.java
+++ domain/src/main/java/server/admin/model/report/dto/request/AppReportUpdateRequest.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import server.admin.model.report.entity.AppReport;
 
 @Getter
-//@JsonInclude(JsonInclude.Include.NON_ABSENT)
-@JsonDeserialize(using = UpdateRequestDeserializer.class)
 public class AppReportUpdateRequest {
     private AppReport.ReportStatus status;
     private Long verifierId;
